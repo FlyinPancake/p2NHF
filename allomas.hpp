64ac2c607bf4c8bd54fef34efd3727e3374ea08f
flyinpancake <flyinpancake@sch.bme.hu>
2020-05-15 21:22:04 +0200

    allomas.hpp megvalositva

--- allomas.hpp
+++ allomas.hpp
@@ -2,15 +2,36 @@
 #define ALLOMAS_HPP
 
 #include <string>
+#include <iostream>
+
 using std::string;
 class Allomas
 {
 public:
-    Allomas(string allomasNev, int sorszam);
-    ~Allomas();
-    bool operator==(const Allomas& rhs);
+    Allomas(string allomasNev) : nev(allomasNev) {}
+
+    bool operator==(const Allomas& rhs)
+    {
+        return (rhs.nev == nev);
+    }
+
+    bool operator!=(const Allomas& rhs)
+    {
+        return (rhs.nev != nev);
+    }
+
+    friend std::ostream &operator<<(std::ostream& os,const Allomas &all)
+    {
+        os << "Allomas: " << all.nev;
+        return os;
+    }
+    
 private:
     string nev;
-};
+};  
+
 
 #endif // !ALLOMAS_HPP
+
+
+
