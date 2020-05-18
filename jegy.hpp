74644c30d8e6f986d9130bde26ca429f335c7cb4
flyinpancake <flyinpancake@sch.bme.hu>
2020-05-18 16:27:15 +0200

    jegy megvalositas

--- jegy.hpp
+++ jegy.hpp
@@ -10,16 +10,50 @@ private:
     Vonat *jarat;
     size_t kocsi;
     size_t ules; 
+    bool validity;
 public:
-    Jegy(Vonat *jarat, size_t kocsi, size_t ules);
-    Jegy(const Jegy &j);
-    ~Jegy();
 
-    Jegy operator=(const Jegy &rhs);
+    /**
+     * Jegy 
+     * 
+     * @param  {Vonat*} jarat : Vonatra mutató ptr.
+     * @param  {size_t} kocsi : hanyadik kocsiba szól a jegy
+     * @param  {size_t} ules  : hányas számú ülésre szól a jegy
+     */
+    Jegy(Vonat *jarat, size_t kocsi, size_t ules):  kocsi(kocsi), 
+                                                    ules(ules), 
+                                                    jarat(jarat),
+                                                    validity(true)
+    {
+        if(!(jarat -> IsVald()))
+        {
+            throw "Nem ervenyes VONAT";
+            return;
+        }
+
+        
+    }
+    
+    /**
+     * 
+     * @param  {Jegy} rhs : 
+     * @return {bool}     : 
+     */
     bool operator==(const Jegy &rhs);
+    
+    /**
+     * 
+     * @param  {Jegy} rhs : 
+     * @return {bool}     : 
+     */
     bool operator!=(const Jegy &rhs);
+
+    /**
+     * 
+     * @param  {std::ostream} os : 
+     * @return {std::ostream}    : 
+     */
     std::ostream operator<<(const std::ostream &os) const;
 };
-
 
 #endif // !JEGY_HPP
